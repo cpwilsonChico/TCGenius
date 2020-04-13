922b4d74e3dd978e60730453dfd874d323ea9437
cpwilsonChico <cpwilson@mail.csuchico.edu>
2020-04-13 16:28:57 -0700

    Implement basic search API

--- lib/scry.dart
+++ lib/scry.dart
@@ -1,20 +1,28 @@
 import 'dart:async';
 import 'dart:convert';
-import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 
 class Data{
   Future<List> getData() async{
     http.Response responce = await http.get(
-        Uri.encodeFull("https://api.scryfall.com/cards/search?order=cmc&q=c:red+pow=3"),
+        Uri.encodeFull("https://api.scryfall.com/cards/named?fuzzy=Aluren"),
         headers: {
           "Accept": "application/json"
         }
     );
-    List data = json.decode(responce.body)['data'];
+    var data = json.decode(responce.body);//['data'];
+    String cardName = data['name'];
+    String manaCost = data['mana_cost'];
+    String oracleText = data['oracle_text'];
+    String setName = data['set_name'];
+    print(cardName);
+    print(manaCost);
+    print(oracleText);
+    print(setName);
+    /*
     for(var ad = 0; ad < data.length; ad++){
       print(data[ad]['name']);
-    }
-    return data;
+    }*/
+    return null;//data;
   }
 }
