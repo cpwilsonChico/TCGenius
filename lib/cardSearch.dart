cf2f4ee0a91dc391bb3ab18c3287be07ad50c9fc
cpwilsonChico <cpwilson@mail.csuchico.edu>
2020-04-15 19:25:57 -0700

    Add basic search bar

--- lib/cardSearch.dart
+++ lib/cardSearch.dart
@@ -11,35 +11,124 @@ class CardSearch extends StatefulWidget {
 class SearchState extends State<CardSearch> {
 
   Data data = new Data();
+  String searchTerm;
   Future<List<MTGCard>> futureCards;
 
   @override
   void initState() {
     super.initState();
-    futureCards = data.getCardsByName("Cat");
+    //futureCards = data.getCardsByName("Cat");
+  }
+
+  void searchCallback(String term) {
+    print("SEARCH CALLBACK");
+    futureCards = data.getCardsByName(term);
+    setState((){});
   }
 
   Widget build(BuildContext context) {
-    return FutureBuilder<List<MTGCard>>(
-      future: futureCards,
-      builder: (BuildContext cont, AsyncSnapshot<List<MTGCard>> snapshot) {
-        if (snapshot.hasData) {
-          var cards = snapshot.data;
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      children: <Widget> [
+        CardListDisplay(futureCards),
+        CardSearchBar(searchCallback),
+      ]
+    );
+  }
+}
 
-          return ListView.builder(
-            itemCount: cards.length,
-            itemBuilder: (context, index) {
-              return Text(cards[index].getName());
-            }
-          );
-        } else {
-          return SizedBox(
-            child: CircularProgressIndicator(),
-            width: 60,
-            height: 60,
-          );
+// displays list of searched cards
+class CardListDisplay extends StatelessWidget {
+  final Future<List<MTGCard>> futureCards;
+  CardListDisplay(this.futureCards);
+
+  Widget build(BuildContext context) {
+    return FutureBuilder<List<MTGCard>>(
+        future: futureCards,
+        builder: (BuildContext cont, AsyncSnapshot<List<MTGCard>> snapshot) {
+          if (snapshot.hasData) {
+            var cards = snapshot.data;
+
+            return Flexible(
+              child: ListView.builder(
+                  itemCount: cards.length,
+                    shrinkWrap: true,
+                    itemBuilder: (context, index) {
+                      return CardListItem(cards[index]);
+                    }
+                )
+            );
+
+          } else {
+            return SizedBox(
+              child: CircularProgressIndicator(),
+              width: 60,
+              height: 60,
+            );
+          }
         }
-      }
+    );
+  }
+}
+
+// each card in the list maps to one of these
+class CardListItem extends StatelessWidget {
+  final MTGCard card;
+  CardListItem(this.card);
+
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.fromLTRB(8, 0, 0, 0),
+      child: Text(card.getName()),
+    );
+  }
+}
+
+// search bar at bottom of page for user input
+class CardSearchBar extends StatefulWidget {
+  final Function callback;
+  CardSearchBar(this.callback);
+  State<CardSearchBar> createState() => CardSearchBarState(callback);
+}
+
+class CardSearchBarState extends State<CardSearchBar> {
+  String currentInput = "";
+  Function callback;
+
+  CardSearchBarState(this.callback);
+
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.all(8.0),
+      //margin: EdgeInsets.all(8.0),
+      child: Row(
+          mainAxisSize: MainAxisSize.min,
+          children: <Widget> [
+            SizedBox(
+              height: 80,
+              width: MediaQuery.of(context).size.width * 0.6,
+              child: TextField(
+                  decoration: InputDecoration(
+                    labelText: "Card Name",
+                    border: OutlineInputBorder(),
+                  ),
+                  onChanged: (String value) {
+                    currentInput = value;
+                    setState((){});
+                  }
+              ),
+            ),
+
+
+            FlatButton(
+                child: Icon(Icons.search),
+                onPressed: () {
+                  callback(currentInput);
+                }
+            ),
+
+          ]
+      )
     );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -37,6 +37,7 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      resizeToAvoidBottomPadding: true,
       drawer: NavDrawer(),
       appBar: AppBar(
         title: Text("TCGenius"),
