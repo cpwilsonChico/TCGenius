e65bfebc74a51f260c1ea8903d638285a0b075ff
cpwilsonChico <cpwilson@mail.csuchico.edu>
2020-04-26 19:55:45 -0700

    Partially implement deck building

--- lib/deckview.dart
+++ lib/deckview.dart
@@ -2,24 +2,98 @@ import 'package:flutter/material.dart';
 import 'cards.dart';
 import 'nav.dart';
 
-class DeckView extends StatefulWidget {
+class DeckViewPage extends StatefulWidget {
+  final Deck deck;
+  DeckViewPage(this.deck);
+  State<DeckViewPage> createState() => DeckPageState(deck);
+}
+
+class DeckPageState extends State<DeckViewPage> {
+  Deck deck;
+  DeckPageState(this.deck);
+
+  Widget build(BuildContext context) {
+    return Text("hi");
+  }
+}
+
+class DeckView extends StatelessWidget {
   final Deck deck;
   DeckView(this.deck);
 
-  State<DeckView> createState() => DeckViewState(deck);
+  Widget build(BuildContext context) {
+    return Container(
+      child: Text("${deck.deckName}"),
+    );
+  }
 }
 
-class DeckViewState extends State<DeckView> {
+class DeckBuilderPage extends StatefulWidget {
+  State<DeckBuilderPage> createState() => DeckBuilderState();
+}
+
+class DeckBuilderState extends State<DeckBuilderPage> {
   Deck deck;
-  DeckViewState(this.deck);
+
+  @override
+  void initState() {
+    super.initState();
+    deck = new Deck.empty();
+  }
 
   Widget build(BuildContext context) {
     return Scaffold(
       drawer: NavDrawer(),
       appBar: AppBar(
-        title: Text("TCGenius"),
+        title: Text("Deck Builder"),
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.cancel),
+            onPressed: () {
+              showCancelDialog(context);
+            }
+          ),
+          IconButton(
+            icon: Icon(Icons.save),
+            onPressed: () {
+              // TODO implement
+              print("Saved.");
+            }
+          )
+        ]
       ),
-      body: Text("${deck.deckName}"),
+      body: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: <Widget>[
+
+        ]
+      )
+    );
+  }
+
+  void showCancelDialog(BuildContext context) {
+    showDialog(
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          title: Text("Clear Deck"),
+          content: Text("Are you sure you want to remove all cards from this deck?"),
+          actions: <Widget>[
+            FlatButton(
+              child: Text("No"),
+              onPressed: () => Navigator.pop(context),
+            ),
+            FlatButton(
+              child: Text("Yes"),
+              onPressed: () {
+                deck.clear();
+                setState((){});
+                Navigator.pop(context);
+              }
+            )
+          ]
+        );
+      }
     );
   }
 }
