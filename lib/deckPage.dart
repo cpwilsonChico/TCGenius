ed5ac9b2536d019e0ee809c5e0b4c997548bb5e0
cpwilsonChico <cpwilson@mail.csuchico.edu>
2020-05-11 17:15:36 -0700

    Allow decks to be sorted

--- lib/deckPage.dart
+++ lib/deckPage.dart
@@ -13,6 +13,7 @@ class DeckPage extends StatefulWidget {
 
 class DeckPageState extends State<DeckPage> {
   Future<List<Deck>> deckFuture;
+  SortOptions sortOption = SortOptions.NAME;
 
   @override
   initState() {
@@ -35,7 +36,7 @@ class DeckPageState extends State<DeckPage> {
           ),
           IconButton(
             icon: Icon(Icons.settings),
-            onPressed: () => print("settings tap"),
+            onPressed: () => showSettingsDialog(context),
           )
         ]
       ),
@@ -56,6 +57,7 @@ class DeckPageState extends State<DeckPage> {
                   ),
                 );
               }
+              sortDeck(decks);
               return Center(
                 child: ListView.builder(
                     itemCount: decks.length,
@@ -83,12 +85,169 @@ class DeckPageState extends State<DeckPage> {
     return await FirebaseDB.instance.getDecks();
   }
 
+  void sortDeck(List<Deck> list) {
+    switch (sortOption) {
+      case SortOptions.NAME:
+        list.sort(compareByName);
+        break;
+      case SortOptions.FORMAT:
+        list.sort(compareByFormat);
+        break;
+      case SortOptions.PRICE:
+        list.sort(compareByPrice);
+        break;
+      default:
+        break;
+    }
+  }
+
+  int compareByName(Deck a, Deck b) {
+    return a.deckName.compareTo(b.deckName);
+  }
+  int compareByFormat(Deck a, Deck b) {
+    return a.getFormat().compareTo(b.getFormat());
+  }
+  int compareByPrice(Deck a, Deck b) {
+    double dif = a.getTotalPrice() - b.getTotalPrice();
+    if (dif < 0) return -1;
+    if (dif > 0) return 1;
+    return 0;
+  }
+
   // callback passed to DeckBuilder
   void refresh() async {
     deckFuture = getDecks();
     setState((){});
   }
+
+  // callback passed to SortChooser
+  void setSortOptions(SortOptions so) {
+    sortOption = so;
+    setState((){});
+  }
+
+  void showSettingsDialog(BuildContext context) {
+    showDialog(
+      context: context,
+      child: AlertDialog(
+        title: Text("Sort by:"),
+        content: SortChooser(setSortOptions, sortOption),
+        actions: <Widget>[
+          FlatButton(
+            child: Text("OK"),
+            onPressed: () => Navigator.pop(context),
+          )
+        ]
+      )
+    );
+  }
 }
+
+enum SortOptions {
+  NAME,
+  FORMAT,
+  PRICE
+}
+
+class SortChooser extends StatefulWidget {
+  final Function sortCallback;
+  final SortOptions initialOption;
+  SortChooser(this.sortCallback, this.initialOption);
+  @override
+  _SortChooserState createState() => _SortChooserState(sortCallback, initialOption);
+}
+
+class _SortChooserState extends State<SortChooser> {
+  final Function sortCallback;    // callback from DeckPage
+  _SortChooserState(this.sortCallback, SortOptions initial) {
+    groupValue = initial;
+  }
+
+  SortOptions groupValue;
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: <Widget>[
+              ListTile(
+                title: Align(
+                  alignment: Alignment.topLeft,
+                  child: FlatButton(
+                    padding: EdgeInsets.all(0),
+                    child: Text("Name"),
+                    splashColor: Colors.transparent,
+                    onPressed: () {
+                      groupValue = SortOptions.NAME;
+                      sortCallback(groupValue);
+                      setState((){});
+                    }
+                  ),
+                ),
+                leading: Radio(
+                  value: SortOptions.NAME,
+                  groupValue: groupValue,
+                  onChanged: (SortOptions val) {
+                    groupValue = val;
+                    sortCallback(val);
+                    setState((){});
+                  },
+                ),
+              ),
+              ListTile(
+                title: Align(
+                  alignment: Alignment.topLeft,
+                  child: FlatButton(
+                      padding: EdgeInsets.all(0),
+                      child: Text("Format"),
+                      splashColor: Colors.transparent,
+                      onPressed: () {
+                        groupValue = SortOptions.FORMAT;
+                        sortCallback(groupValue);
+                        setState((){});
+                      }
+                  ),
+                ),
+                leading: Radio(
+                  value: SortOptions.FORMAT,
+                  groupValue: groupValue,
+                  onChanged: (SortOptions val) {
+                    groupValue = val;
+                    sortCallback(val);
+                    setState((){});
+                  }
+                )
+              ),
+              ListTile(
+                title: Align(
+                  alignment: Alignment.topLeft,
+                  child: FlatButton(
+                      padding: EdgeInsets.all(0),
+                      child: Text("Price"),
+                      splashColor: Colors.transparent,
+                      onPressed: () {
+                        groupValue = SortOptions.PRICE;
+                        sortCallback(groupValue);
+                        setState((){});
+                      }
+                  ),
+                ),
+                leading: Radio(
+                  value: SortOptions.PRICE,
+                  groupValue: groupValue,
+                  onChanged: (SortOptions val) {
+                    groupValue = val;
+                    sortCallback(val);
+                    setState((){});
+                  }
+                )
+              )
+            ]
+        )
+    );
+  }
+}
+
 
 class DeckTile extends StatelessWidget {
   final Deck deck;
